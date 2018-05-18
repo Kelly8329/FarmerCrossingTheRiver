a8a8ba30beded897dda0ef7541f1b68cc97276a4
育瑄 張 <kellyjhang8329@gmail.com>
2018-05-18 13:50:29 +0800

    更新

---
+++ FarmerCrossingTheRiver/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace FarmerCrossingTheRiver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button button1;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnRestart = new System.Windows.Forms.Button();
+            button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            button1.BackColor = System.Drawing.Color.Transparent;
+            button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            button1.ForeColor = System.Drawing.Color.Transparent;
+            button1.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            button1.Location = new System.Drawing.Point(367, 241);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(168, 156);
+            button1.TabIndex = 2;
+            button1.TabStop = false;
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(172)))), ((int)(((byte)(24)))));
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 31;
+            this.listBox1.Location = new System.Drawing.Point(41, 240);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 124);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(172)))), ((int)(((byte)(24)))));
+            this.listBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox2.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 26;
+            this.listBox2.Location = new System.Drawing.Point(627, 209);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 104);
+            this.listBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(63, 189);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 35);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "左岸";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(650, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 35);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "右岸";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(341, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 31);
+            this.label3.TabIndex = 6;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnRestart.Location = new System.Drawing.Point(627, 22);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(75, 23);
+            this.btnRestart.TabIndex = 7;
+            this.btnRestart.Text = "重新開始";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(770, 414);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(button1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "農夫過河";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnRestart;
+    }
+}
+
---
+++ FarmerCrossingTheRiver/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FarmerCrossingTheRiver
+{
+    public partial class Form1 : Form
+    {
+        private List<MyData> _leftList;
+        private List<MyData> _rightList;
+        public static int count = -1;
+
+
+        public Form1()
+        {
+            _leftList = CreateList();
+            _rightList = new List<MyData>();
+            InitializeComponent();
+            SetListBoxDataSource();
+            ChangeDate();
+        }
+
+        private static List<MyData> CreateList()
+        {
+            var list = new List<MyData>();
+            list.Add(new MyData() { ItemNumber = 0, Item = "農夫" });
+            list.Add(new MyData() { ItemNumber = 1, Item = "草" });
+            list.Add(new MyData() { ItemNumber = 2, Item = "羊" });
+            list.Add(new MyData() { ItemNumber = 3, Item = "狼" });
+            return list;
+        }
+
+        private void SetListBoxDataSource()//設定兩個ListBox的SelectionMode
+        {
+            listBox1.SelectionMode = SelectionMode.MultiSimple;
+            listBox2.SelectionMode = SelectionMode.MultiSimple;
+        }
+
+        private void ChangeDate()//設定兩個ListBox的資料來源
+        {
+            var leftNumber = _leftList.OrderBy((x) => x.ItemNumber);
+            var rightNumber = _rightList.OrderBy((x) => x.ItemNumber);
+            listBox1.DataSource = null;
+            listBox2.DataSource = null;
+            listBox1.DataSource = leftNumber.ToList();
+            listBox1.DisplayMember = "Item";
+            listBox2.DataSource = rightNumber.ToList();
+            listBox2.DisplayMember = "Item";
+            count += 1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (count % 2 == 0) //判斷哪一邊(左邊)
+            {
+                if (listBox1.SelectedItem == null)
+                {
+                    MessageBox.Show("必須選擇一個項目，最多不能超過二個項目");
+                }
+                else if (((MyData)listBox1.SelectedItem).Item != "農夫")//農夫不在
+                    MessageBox.Show("物品不能自己過河");
+
+                else if (listBox1.SelectedItems.Count < 3 && listBox1.SelectedItems.Count > 0)//判斷選擇的項目數，必須1<數目<3
+                {
+                    foreach (var i in listBox1.SelectedItems)
+                    {
+                        _leftList.Remove((MyData)i);
+                        _rightList.Add((MyData)i);
+                    }
+                    ChangeDate();
+                    
+                    if (((List<MyData>)listBox1.DataSource).Any((x) => x.Item == "狼")
+                                && ((List<MyData>)listBox1.DataSource).Any((x) => x.Item == "草")
+                                && ((List<MyData>)listBox1.DataSource).Any((x) => x.Item == "羊"))
+                    {
+                        MessageBox.Show("Game Over！！羊吃掉草，狼吃掉羊");
+                        this.Close();
+                    }
+                    else if (((List<MyData>)listBox1.DataSource).Any((x) => x.Item == "草")
+                                && ((List<MyData>)listBox1.DataSource).Any((x) => x.Item == "羊"))
+                    {
+                        MessageBox.Show("Game Over！！羊吃掉草");
+                        this.Close();
+                    }
+
+                    else if (((List<MyData>)listBox1.DataSource).Any((x) => x.Item == "狼")
+                        && ((List<MyData>)listBox1.DataSource).Any((x) => x.Item == "羊"))
+                    {
+                        MessageBox.Show("Game Over！！狼吃掉羊");
+                        this.Close();
+                    }
+                    else if (((List<MyData>)listBox2.DataSource).Any((x) => x.Item == "狼")
+                                && ((List<MyData>)listBox2.DataSource).Any((x) => x.Item == "草")
+                                && ((List<MyData>)listBox2.DataSource).Any((x) => x.Item == "羊")
+                                && ((List<MyData>)listBox2.DataSource).Any((x) => x.Item == "農夫"))
+                    {
+                        MessageBox.Show("WIN!");
+                    }
+                }
+                else
+                    MessageBox.Show("不能超過二個項目");
+            }
+            else//(右邊)
+            {
+                if (listBox2.SelectedItem == null)
+                {
+                    MessageBox.Show("必須選擇一個項目，最多不能超過二個項目");
+                }
+                else if (((MyData)listBox2.SelectedItem).Item != "農夫")//農夫不在
+                    MessageBox.Show("物品不能自己過河");
+
+                else if (listBox2.SelectedItems.Count < 3 || listBox2.SelectedItems.Count > 0)//判斷選擇的項目數，必須1<數目<3
+                {
+                    foreach (var i in listBox2.SelectedItems)
+                    {
+                        _leftList.Add((MyData)i);
+                        _rightList.Remove((MyData)i);
+                    }
+                    ChangeDate();
+
+                    if (((List<MyData>)listBox2.DataSource).Any((x) => x.Item == "狼")
+                                && ((List<MyData>)listBox2.DataSource).Any((x) => x.Item == "草")
+                                && ((List<MyData>)listBox2.DataSource).Any((x) => x.Item == "羊"))
+                    {
+                        MessageBox.Show("Game Over！！羊吃掉草，狼吃掉羊");
+                        this.Close();
+                    }
+                    
+                    else if (((List<MyData>)listBox2.DataSource).Any((x) => x.Item == "草")
+                                && ((List<MyData>)listBox2.DataSource).Any((x) => x.Item == "羊"))
+                    {
+                        MessageBox.Show("Game Over！！羊吃掉草");
+                        this.Close();
+                    }
+
+                    else if (((List<MyData>)listBox2.DataSource).Any((x) => x.Item == "狼")
+                               && ((List<MyData>)listBox2.DataSource).Any((x) => x.Item == "羊"))
+                    {
+                        MessageBox.Show("Game Over！！狼吃掉羊");
+                        this.Close();
+                    }
+                }
+                else
+                    MessageBox.Show("不能超過二個項目");
+            }
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            count = -1;
+            _leftList = CreateList();
+            _rightList = new List<MyData>();
+            SetListBoxDataSource();
+            ChangeDate();
+        }
+    }
+}
